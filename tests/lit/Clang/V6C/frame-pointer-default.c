a23991e51c406201f64eca1cdc1a9852bfeed293
parallelno <mailforfriendparallelno@gmail.com>
2026-04-10 01:13:08 -0700

    Fix clang command syntax in frame pointer test files

--- tests/lit/Clang/V6C/frame-pointer-default.c
+++ tests/lit/Clang/V6C/frame-pointer-default.c
@@ -1,4 +1,4 @@
-// RUN: %clang -target i8080-unknown-v6c -O2 -S -emit-llvm %s -o - | FileCheck %s
+// RUN: clang -target i8080-unknown-v6c -O2 -S -emit-llvm %s -o - | FileCheck %s
 // CHECK-NOT: "frame-pointer"="all"
 // CHECK-NOT: "frame-pointer"="non-leaf"
 int simple(int x) { return x + 1; }
