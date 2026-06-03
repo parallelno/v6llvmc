7207e86e568b645770513928a90192b90267f1fb
parallelno <mailforfriendparallelno@gmail.com>
2026-06-02 23:29:07 -0700

    Enhance drawing functions: improve fill_rect logic and add draw_text support for extended characters

--- samples/03_demo/main.c
+++ samples/03_demo/main.c
@@ -21,7 +21,6 @@
 #include <v6c_draw.h>
 #include <v6c_math.h>
 
-#include "include/font.h"
 
 #define DRAW_NEW
 
@@ -40,39 +39,42 @@ void main() {
     // clean up the screen.
     v6c_set_palette(palette + PALETTE_LEN - 1);
 
-    // memset(SCR_BUFF0_PTR, 0x00, SCR_BUFF_LEN * 4);
-    // // fill_rect(8, 50, 16, 156);
-    // // draw_text("HELLO WORLD", 10, 10);
+    memset(SCR_BUFF0_PTR, 0x00, SCR_BUFF_LEN * 4);
 
-    // // Draw 100 lines from the center of the screen to random points.
-    // for (int i = 0; i < LINES; i++) {
-    //     uint16_t r1 = rand();
-    //     uint8_t x1 = r1 & 0xFF;
-    //     uint8_t y1 = (r1 >> 8);
-    // #ifdef DRAW_NEW
-    //     draw_line(SCR_BUFF0_ADDR_H, 127, 127, x1, y1);
-    // #else
-    //     draw_line_old(127, 127, x1, y1, SCR_ADDR_PTR);
-    // #endif
-    // }
+    // draw_text("HELLO WORLD", 10, 10);
 
-    // // Draw 10 circles at 127, 127 from 100 radius, each smaller by 10.
-    // for (int i = 0; i < 10; i++) {
-    //     draw_circle(127, 127, 100 - i * 10, SCR_BUFF1_ADDR_H);
-    // }
+    // Draw 100 lines from the center of the screen to random points.
+    for (int i = 0; i < LINES; i++) {
+        uint16_t r1 = rand();
+        uint8_t x1 = r1 & 0xFF;
+        uint8_t y1 = (r1 >> 8);
+        draw_line(SCR_BUFF0_ADDR_H, 127, 127, x1, y1);
+    }
 
-    // // Draw a sin wave across the screen across all 4 planes to test palette and different scr_addr_hi.
-    // for (uint8_t i = 0; i <= 10; i++) {
-    //     for (int x = 0; x < 256; x++) {
-    //         uint8_t y = 127 + sin8(x + i) / 4 + i;
-    //         draw_pixel(x, y, SCR_BUFF0_ADDR_H + ((i & 3) << 5));
-    //     }
-    // }
+    // Draw 10 circles at 127, 127 from 100 radius, each smaller by 10.
+    for (int i = 0; i < 10; i++) {
+        draw_circle(127, 127, 100 - i * 10, SCR_BUFF1_ADDR_H);
+    }
+
+    // Draw a sin wave across the screen across all 4 planes to test palette and different scr_addr_hi.
+    for (uint8_t i = 0; i <= 10; i++) {
+        for (int x = 0; x < 256; x++) {
+            uint8_t y = 127 + sin8(x + i) / 4 + i;
+            draw_pixel(x, y, SCR_BUFF0_ADDR_H + ((i & 3) << 5));
+        }
+    }
 
     // Draw rectangles to test fill_rect.
-    // for (int i = 0; i < 3; i++) {
-    //     fill_rect(40 - i * 4, 60 - i * 8, 200 - i * 8, 32, SCR_BUFF0_ADDR_H + ((i & 3) << 5));
-    // }
-    // test reqtangle
-    fill_rect(129, 128, 32, 32, SCR_BUFF0_ADDR_H);
+    for (int i = 0; i < 3; i++) {
+        fill_rect(40 - i * 4, 60 - i * 8, 200 - i * 8, 32, SCR_BUFF0_ADDR_H + ((i & 3) << 5));
+    }
+
+    // Draw text to test draw_text and font.
+    #define TEXT_X 64
+    #define TEXT_Y 240
+    #define TEXT_DY 12
+    draw_text(" !\"#$%&'()*+,-./", TEXT_X, TEXT_Y, SCR_BUFF2_ADDR_H);
+    draw_text("0123456789:;<=>?", TEXT_X, TEXT_Y - TEXT_DY, SCR_BUFF2_ADDR_H);
+    draw_text("@ABCDEFGHIJKLMNO", TEXT_X, TEXT_Y - 2 * TEXT_DY, SCR_BUFF2_ADDR_H);
+    draw_text("PQRSTUVWXYZ", TEXT_X, TEXT_Y - 3 * TEXT_DY, SCR_BUFF2_ADDR_H);
 }
